fd96785225d788c076de1101b7a1707fe175a2ce
avd21 <74905410+avd21@users.noreply.github.com>
2021-05-09 00:48:29 +0530

    first commit

---
+++ str_manupulation/simple.java
@@ -0,0 +1,16 @@
+package string;
+
+import java.util.Scanner;
+
+public class simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1;
+		Scanner s1=new Scanner(System.in);
+		System.out.println("Enter any string:");
+		str1=s1.nextLine();
+		System.out.println(str1);
+	}
+
+}
